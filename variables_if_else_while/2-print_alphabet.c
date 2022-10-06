ac8c221fafc14089cf86f8d548ecf37c6cad0a6a
DESQUIMOR <d.esquimor@gmail.com>
2022-10-06 10:37:06 -0600

    alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter\n);
+
+	return (0);
+}
