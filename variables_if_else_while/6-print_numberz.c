c69bc525c219c23cdadc21b70e66dfceed5e6815
Diego Esquivel <desquimor@Hogwarts.localdomain>
2024-02-24 09:05:19 -0600

    correct exercise five and 6-print_numberz

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Print numbers of base 10 starting from 0.
+ * main - Prints all single digit numbers of base 10 starting from 0.
  *
  * Return: Always 0.
  */
@@ -10,9 +10,9 @@ int main(void)
 	int num;
 
 	for (num = 0; num < 10; num++)
-		printf("%d", num);
+		putchar((num % 10) + '0');
 
-	printf("\n");
+	putchar('\n');
 
 	return (0);
 }
