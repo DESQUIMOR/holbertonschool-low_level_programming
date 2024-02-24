ca6c6e6c66369e464f0b32b3a0f00f1e192e69c0
Diego Esquivel <desquimor@Hogwarts.localdomain>
2024-02-24 08:38:56 -0600

    updated 101-quote

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,14 +1,14 @@
+#include <unistd.h>
 #include <stdio.h>
 
 /**
  * main - Entry point
  *
- * Return: Always 1
+ * Return: Always 1 (Error code)
  */
 int main(void)
 {
-	fprintf(stderr,
-		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-
-	return (1);
+    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+    
+    return 1;
 }
