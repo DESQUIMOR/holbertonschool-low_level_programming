27c4f48942b91216ffe03ea2620550e29772f374
Diego Esquivel <desquimor@Hogwarts.localdomain>
2024-02-24 09:21:04 -0600

    10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * add - Adds two values
+  * @n1: The first number in operation.
+  * @n2: The second number in operation.
+  *
+  * Return: Integer value (sum result)
+  */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
