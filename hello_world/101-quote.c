2c75f79c6771914f4e7a36bddb9bf1c89240cc0a
Diego Esquivel <desquimor@Hogwarts.localdomain>
2024-10-23 15:23:15 -0600

    fix 101-quote.c

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,14 +1,18 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
  *
- * Return: Always 1
+ * Description: Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19
+ * to the standard error.
+ *
+ * Return: Always 1 (Error)
  */
 int main(void)
 {
-	fprintf(stderr,
-		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
+	write(2, msg, sizeof(msg) - 1);
 	return (1);
 }
+
