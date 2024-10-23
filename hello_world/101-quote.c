c478421fc189834cf71995c3941388f679c94059
Diego Esquivel <desquimor@Hogwarts.localdomain>
2024-10-23 15:26:13 -0600

    fix betty style in 101-quote.c

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -3,8 +3,8 @@
 /**
  * main - Entry point
  *
- * Description: Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19
- * to the standard error.
+ * Description: Prints "and that piece of art is useful" - Dora Korpar,
+ * 2015-10-19" to the standard error.
  *
  * Return: Always 1 (Error)
  */
@@ -15,4 +15,3 @@ int main(void)
 	write(2, msg, sizeof(msg) - 1);
 	return (1);
 }
-
