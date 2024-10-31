53aa99a251d52d273c97aac86de917aebd6ee5cb
Diego Esquivel <desquimor@Hogwarts.localdomain>
2024-10-31 17:10:45 -0600

    fix 101-print_number.c

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -1,20 +1,27 @@
 #include "main.h"
 
 /**
- * print_number - prints an integer using _putchar
- * @n: the integer to print
+ * print_number - imprime un número entero usando _putchar
+ * @n: el número entero a imprimir
  */
-
 void print_number(int n)
 {
-	if (n < 0)
-	{
-		_putchar('-');
-		n *= -1;
-	}
+    if (n == -2147483648)
+    {
+        _putchar('-');
+        _putchar('2');
+        print_number(147483648);
+        return;
+    }
 
-	if (n / 10 != 0)
-		print_number(n / 10);
+    if (n < 0)
+    {
+        _putchar('-');
+        n = -n;
+    }
 
-	_putchar((n % 10) + '0');
+    if (n / 10 != 0)
+        print_number(n / 10);
+
+    _putchar((n % 10) + '0');
 }
